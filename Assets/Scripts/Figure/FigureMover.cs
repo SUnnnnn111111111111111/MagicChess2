583a3ffa1d3a3c3a61082060183a7b38fb9e1e9a
SUnnnnn111111111111111 <oleg.grishaev02@mail.ru>
2025-03-12 09:29:54 +0300

    Доработана анимация передвижения, исправлена ошибка

--- Assets/Scripts/Figure/FigureMover.cs
+++ Assets/Scripts/Figure/FigureMover.cs
@@ -4,17 +4,19 @@ using System.Collections;
 
 public class FigureMover : MonoBehaviour
 {
-    [SerializeField] private float moveDuration = 0.5f; 
-    [SerializeField] private AnimationCurve moveCurve = AnimationCurve.EaseInOut(0, 0, 1, 1); 
-    [SerializeField] private float rotateDuration = 0.2f; 
+    [SerializeField] private float moveDuration = 0.5f;
+    [SerializeField] private float rotateDuration = 0.2f;
+    [SerializeField] private Ease moveEase = Ease.OutQuad; 
+    [SerializeField] private float jumpPower = 1.0f; 
+    [SerializeField] private int jumpCount = 1; 
 
     private Figure figure;
-    private Quaternion originalRotation; 
+    private Quaternion originalRotation;
 
     private void Start()
     {
         figure = GetComponent<Figure>();
-        originalRotation = figure.transform.rotation; 
+        originalRotation = figure.transform.rotation;
     }
 
     public void MoveToTile(Tile targetTile)
@@ -30,7 +32,6 @@ public class FigureMover : MonoBehaviour
         {
             return;
         }
-        
 
         if (targetTile.OccupyingFigure != null && targetTile.OccupyingFigure.whiteTeamAffiliation != figure.whiteTeamAffiliation)
         {
@@ -51,25 +52,27 @@ public class FigureMover : MonoBehaviour
         {
             currentTile.SetOccupyingFigure(null);
         }
-        
+
         originalRotation = figure.transform.rotation;
-        
+
         Vector3 direction = (targetTile.transform.position - figure.transform.position).normalized;
         Vector3 lookAtPosition = figure.transform.position + direction;
+
+        HighlightTilesController.Instance.ClearHighlights();
         
         figure.transform.DOLookAt(lookAtPosition, rotateDuration, AxisConstraint.Y)
             .OnComplete(() =>
             {
-                figure.transform.DOMove(targetTile.transform.position, moveDuration)
-                    .SetEase(moveCurve) 
+                
+                figure.transform.DOJump(targetTile.transform.position, jumpPower, jumpCount, moveDuration)
+                    .SetEase(moveEase) 
                     .OnComplete(() =>
                     {
                         figure.SetCurrentTile(targetTile);
                         targetTile.SetOccupyingFigure(figure);
-                        
+
                         figure.transform.DORotateQuaternion(originalRotation, rotateDuration);
 
-                        HighlightTilesController.Instance.ClearHighlights();
                         GameManager.Instance.SelectedFigure = null;
                     });
             });
