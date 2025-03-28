e24357f77962421426cc15c9a20ea675388155c8
SUnnnnn111111111111111 <oleg.grishaev02@mail.ru>
2025-03-29 00:08:30 +0300

    До рефакторинга ui

--- Assets/_Scripts/Bootstrappers/GameSettingsBootstrapper.cs
+++ Assets/_Scripts/Bootstrappers/GameSettingsBootstrapper.cs
@@ -5,37 +5,49 @@ using UnityEngine.SceneManagement;
 public class GameSettingsBootstrapper : MonoBehaviour
 {
     [SerializeField] private string mainGameSceneName = "MainGameScene";
-    
+
+    private enum BoardType { Default, Test }
+    private static BoardType boardToLoad = BoardType.Default;
+
     private void Start()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
-    
+
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        BoardFactory.Instance.LoadDefaultBoard();
-        // BoardFactory.Instance.LoadTestBoard();
+        switch (boardToLoad)
+        {
+            case BoardType.Default:
+                BoardFactory.Instance.LoadDefaultBoard();
+                break;
+            case BoardType.Test:
+                BoardFactory.Instance.LoadTestBoard();
+                break;
+        }
+
         UIFactory.Instance.LoadGameStateUI();
         UIManager.Instance.BindToGameStateManager();
         EventTriggeringTileManager.Instance.ReplaseTiles();
-        
-        
+
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
-    
-    public void LaunchSettingsUI()
+
+    public void LaunchSettings()
     {
         Debug.Log("[GameSettingsBootstrapper] SettingsUI режим еще не реализован.");
     }
-    
+
     public void LaunchGameWithBlackAIEnemy()
     {
+        boardToLoad = BoardType.Default;
+
         if (GameStateManager.Instance != null)
         {
             GameStateManager.Instance.CurrentGameMode = GameStateManager.GameMode.VsAiEnemy;
             GameStateManager.Instance.humanPlaysWhite = true;
         }
-        
+
         if (AIEnemy.Instance == null)
         {
             GameObject aiObj = Instantiate(Resources.Load("Prefabs/AIEnemy")) as GameObject;
@@ -49,11 +61,14 @@ public class GameSettingsBootstrapper : MonoBehaviour
         {
             Debug.LogWarning("[GameSettingsBootstrapper] AIEnemy уже существует.");
         }
+
         SceneManager.LoadScene(mainGameSceneName);
     }
-    
+
     public void LaunchGameWithWhiteAIEnemy()
     {
+        boardToLoad = BoardType.Default;
+
         if (GameStateManager.Instance != null)
         {
             GameStateManager.Instance.CurrentGameMode = GameStateManager.GameMode.VsAiEnemy;
@@ -63,7 +78,7 @@ public class GameSettingsBootstrapper : MonoBehaviour
         {
             Debug.LogWarning("[GameSettingsBootstrapper] GameStateManager не найден!");
         }
-        
+
         if (AIEnemy.Instance == null)
         {
             GameObject aiObj = Instantiate(Resources.Load("Prefabs/AIEnemy")) as GameObject;
@@ -77,23 +92,33 @@ public class GameSettingsBootstrapper : MonoBehaviour
         {
             Debug.LogWarning("[GameSettingsBootstrapper] AIEnemy уже существует.");
         }
+
         SceneManager.LoadScene(mainGameSceneName);
     }
-    
+
     public void LaunchLocalMultiplayer()
     {
+        boardToLoad = BoardType.Default;
+
         if (GameStateManager.Instance != null)
         {
             GameStateManager.Instance.CurrentGameMode = GameStateManager.GameMode.LocalMultiplayer;
         }
+
         SceneManager.LoadScene(mainGameSceneName);
     }
-    
+
     public void LaunchOnlineMultiplayer()
     {
         Debug.Log("[GameSettingsBootstrapper] OnlineMultiplayer еще не реализован.");
     }
-    
+
+    public void LaunchTestGame()
+    {
+        boardToLoad = BoardType.Test;
+        SceneManager.LoadScene(mainGameSceneName);
+    }
+
     public void ExitGame()
     {
         #if UNITY_EDITOR
